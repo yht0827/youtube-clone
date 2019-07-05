6a764d859dc04cf50373035cbd9fa7b003446d42
yht0827 <yangheetae0827@naver.com>
2019-07-05 22:04:10 +0900

    190705

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -19,17 +19,30 @@ export const home = async (req, res) => {
     }
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
     const {
         query: {
             term: searchingBy
         }
     } = req;
     // const searchingBy = req.query.term;
+    let videos = [];
+
+    try {
+        videos = await Video.find({
+            title: {
+                $regex: searchingBy,
+                $options: "i"
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
 
     res.render("search", {
         pageTitle: "Search",
-        searchingBy
+        searchingBy,
+        videos
     });
 };
 
