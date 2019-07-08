464d6fec20469de916db5fbc735e30358e20b9d6
yht0827 <yangheetae0827@naver.com>
2019-07-08 22:57:16 +0900

    190708

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require("path");
 
+const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 const config = {
   entry: ENTRY_FILE,
+  mode: MODE,
   output: {
     path: OUTPUT_DIR,
     filename: "[name].[format]"
